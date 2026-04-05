43714ddac34ce3396307ffb30f5536034a2bdb43
gc.bigcas <gc.bigcas@gmail.com>
2026-04-05 11:34:08 +0800

    Fixed merging conflict

--- HangarSystem/src/Hangar/Main.java
+++ HangarSystem/src/Hangar/Main.java
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-import UI.ReservationUI;
-=======
 package Hangar;
 
+import UI.ReservationUI;
 import UI.HangarSlotUI;
->>>>>>> origin/feature/Hangar-and-Slot-Configuration
 import java.util.Scanner;
 
 public class Main {
@@ -12,17 +9,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-<<<<<<< HEAD
         String loggedInUser = "gian";
         String userRole     = "FRONT DESK";
 
         new ReservationUI(scanner, loggedInUser, userRole).run();
-=======
-        String loggedInUser = "admin";
-        String userRole     = "ADMIN";
-
         new HangarSlotUI(scanner, loggedInUser, userRole).run();
->>>>>>> origin/feature/Hangar-and-Slot-Configuration
 
         scanner.close();
     }
