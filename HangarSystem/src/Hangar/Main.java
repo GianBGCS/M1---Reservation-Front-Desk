558f7dd9c72790a0ea58d7cae2cae927e070b29e
gc.bigcas <gc.bigcas@gmail.com>
2026-04-05 14:03:58 +0800

    Fixed merging conflict

--- HangarSystem/src/Hangar/Main.java
+++ HangarSystem/src/Hangar/Main.java
@@ -1,9 +1,13 @@
-package Hangar;
+package src.Hangar;
 
-<<<<<<< HEAD
 import UI.ReservationUI;
 import UI.HangarSlotUI;
 import java.util.Scanner;
+import Hangar.DAO.UserDAO;
+import Hangar.Model.User;
+import Hangar.UI.LoginMenu;
+import Hangar.Service.AuthService;
+import Hangar.Utils.PasswordUtils;
 
 public class Main {
 
@@ -17,20 +21,10 @@ public class Main {
         new HangarSlotUI(scanner, loggedInUser, userRole).run();
 
         scanner.close();
-=======
-import Hangar.DAO.UserDAO;
-import Hangar.Model.User;
-import Hangar.UI.LoginMenu;
-import Hangar.Service.AuthService;
-import Hangar.Utils.PasswordUtils;
-
-public class Main {
-    public static void main(String[] args) {
         UserDAO userDAO = new UserDAO();
 
         AuthService authService = new AuthService(userDAO);
         LoginMenu loginMenu = new LoginMenu(authService);
         loginMenu.display();
->>>>>>> ui/mainmenu
     }
 }
