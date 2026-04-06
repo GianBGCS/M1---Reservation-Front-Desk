672a634fe43b5e3fd3b9a326b0e9dcc87e1196f2
Al Jay Wenceslao <aljaywenceslao12@gmail.com>
2026-04-06 09:48:54 +0800

    Added Handle Walk-in Aircraft
    Adding the feature and edit some of the reservation and reservationDAO class so the Front Desk Operation will work.

---
+++ HangarSystem/src/HangarSystem/FrontDeskMain.java
@@ -0,0 +1,18 @@
+package HangarSystem;
+
+import HangarSystem.UI.FrontDeskUI;
+
+/**
+ * ═══════════════════════════════════════════════════
+ * FILE    : FrontDeskMain.java
+ * PACKAGE : HangarSystem
+ * ═══════════════════════════════════════════════════
+ *
+ * Entry point for Front Desk Operations.
+ */
+public class FrontDeskMain {
+
+    public static void main(String[] args) {
+        new FrontDeskUI().start();
+    }
+}
